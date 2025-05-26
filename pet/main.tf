83e3f6214dffb23b4d55f0b87d66aae3ca7ca0b6
Yakul Garg <2000yeshu@gmail.com>
2025-05-26 12:29:41 +0530

    test
    Signed-off-by: Yakul Garg <2000yeshu@gmail.com>

--- pet/main.tf
+++ pet/main.tf
@@ -4,14 +4,14 @@ variable "prefixes" {
 
 # For each prefix, generate a random pet name
 resource "random_pet" "this" {
-  # count =  var.prefixes
+  count =  var.prefixes
   prefix = "pet"
   length = 3
 }
 
 output "name" {
-  value = random_pet.this.id
-  # value = [for n in random_pet.this: n.id]
+  # value = random_pet.this.id
+  value = [for n in random_pet.this: n.id]
 }
 
 
